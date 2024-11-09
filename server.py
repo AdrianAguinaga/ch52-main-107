9ee663c042d3bbf2b8506efac3fb14786a757db2
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2024-11-09 11:49:20 -0800

    first commit

---
+++ server.py
@@ -0,0 +1,81 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "hello from flask"
+# this is just an example
+# @app.post("/")
+# def homePost():
+# return "hello from flask post"
+
+# Endpoints
+@app.get("/test")
+def test():
+    return "hello from the test server"
+
+# Endpoint using Json
+@app.get("/api/about")
+def aboutGet():
+    name = {"name": "Adrian"}
+    return json.dumps(name)
+
+# create a new route /greet/{name}, this route should accept name 
+# as part of the url and return an html message saying hello {name}
+
+@app.get("/greet/<name>")
+def greet(name):
+    return f"""
+<h1 style=color:blue>Hello {name}!</h1>"""
+
+# by creating the farewell message
+@app.get("/farewell/<name>")
+def farewell(name):
+    return f"""
+<h1 style=color:red>bye bye {name}!</h1>"""
+
+# ################################################
+products = []
+@app.get("/api/products")
+def get_products():
+    return json.dumps(products)
+
+@app.post("/api/products")
+def save_products():
+    item = request.get_json()
+    print(item)
+    products.append(item)
+    return json.dumps(item)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    updated_item=request.get_json()
+    if 0<= index <=len(products):
+        products[index] = updated_item
+        return json.dumps(updated_item)
+    else:
+        return "That index does not exist"
+
+@app.delete("/api/products/<int:index>")
+def delete_products(index):
+    delete_item = request.get_json()
+    if 0<= index <=len(products):
+        delete_item = products.pop(index)
+        return json.dumps(delete_item)
+    else:
+        return "That index does not exist"
+
+# pacth -- the method to update an especific element into python is: list.update
+
+@app.patch("/api/products/<int:index>")
+def patch_products(index):
+    updated_field = request.get_json()
+    if 0<= index <=len(products):
+        updated_field(index).update(updated_field)
+        return json.dumps(updated_field)
+    else:
+        return "That index does not exist"
+
+app.run(debug=True)
